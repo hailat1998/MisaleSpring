64365612f1a7121aabc9da89d39833bf1535cfc8
hailat1998 <htemesgen400@gmail.com>
2024-03-14 07:41:16 +0000

    added controller

--- src/main/java/com/hd/misale/controller/HomeController.java
+++ src/main/java/com/hd/misale/controller/HomeController.java
@@ -1,9 +1,13 @@
 package com.hd.misale.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HomeController {
-
+    @GetMapping("/")
+    public String Home(){
+        return "home";
+    }
 
 }
