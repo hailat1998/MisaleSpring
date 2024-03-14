b17a32d8813a72b6ec5191b3dc41696b73fd55e4
hailat1998 <htemesgen400@gmail.com>
2024-03-14 07:00:08 +0000

    initial commit

---
+++ src/main/java/com/hd/misale/MisaleApplication.java
@@ -0,0 +1,14 @@
+package com.hd.misale;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MisaleApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(MisaleApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/hd/misale/controller/HomeController.java
@@ -0,0 +1,9 @@
+package com.hd.misale.controller;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class HomeController {
+
+
+}
